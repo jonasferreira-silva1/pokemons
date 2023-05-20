67cf38fe8f436c5722f5dfd91c0a4f42d586b1b7
jonas <jonas.fsilva1@hotmail.com>
2023-05-20 16:15:44 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
-function App() {
+import './styles.css';
+
+
+
+export default function App() {
+  const [pokemons, setPokemons] = useState([]);
+  const [picture, setPicture] = useState("[]");
+
+  const getPokemons = async () => {
+    try {
+      const response = await axios.get(
+        "https://pokeapi.co/api/v2/pokemon?limit=151&offset=0" 
+      );
+      
+      setPokemons(response.data.results);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const getPokePicture = async (event) => {
+    console.log(event.target.value);
+    try {
+      const url = event.target.value;
+      const response = await axios.get(`${url}`);
+      setPicture(response.data.sprites.front_default);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const pokeList = pokemons.map((poke) => {
+    return (
+      <option key={poke.name} value={poke.url}>
+        {poke.name}
+      </option>
+    );
+  });
+
+  useEffect(() => {
+    getPokemons();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pokemons</h1>
+      <select onChange={getPokePicture}>
+        <option>Selecione um pokemon</option>
+        {pokeList}
+      </select>
+      {picture && <img src={picture} alt="foto do pokemon" />}
     </div>
   );
 }
+  
 
-export default App;
+
+
